56edddd6bcb064dc3d15eb933c5c9b2621773b6a
Italo Vinicius <italovinic884@gmail.com>
2026-04-22 18:50:08 -0300

    Update italoALLPatv02.php

--- italoALLPatv02.php
+++ italoALLPatv02.php
@@ -23,14 +23,17 @@ switch($recomendar){
 case "residencial":
 echo "Equipamento Indicado: Roteador Wireless (SOHO). 
 Para ambientes residenciais, um roteador Wi-Fi padrão é suficiente para conectar smartphones, TVs e notebooks";
+break;
 
 case "pequeno_departamento":
 echo "Equipamento Indicado: Switch Gerenciável de 24 Portas.
 Para um escritório, recomendamos o uso de um Switch para conectar os computadores via cabo, garantindo estabilidade e segurança na rede local (LAN).";
+break;
 
 case "data_center":
 echo "Equipamento Indicado: Roteador de Borda e Switch Layer 3.
 Para Data Centers, é necessária uma infraestrutura robusta com equipamentos de alta capacidade para lidar com grande tráfego de dados e roteamento avançado.";
+break;
 
 default:
 echo"Erro: Ambiente não reconhecido em nosso banco de dados.";
